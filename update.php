83c13e9ce754f0055352d513c66f4c66bead5453
CGMedina03 <medinachristiangenesis@gmail.com>
2023-06-18 18:38:26 +0800

    finishing small ups

--- update.php
+++ update.php
@@ -97,8 +97,9 @@ if (isset($_POST['submit'])) {
                 <label class="form-label">Password</label>
                 <div class="position-relative">
                     <input type="password" id="password" class="form-control" placeholder="Enter your password" name="password" autocomplete="off" value="<?php echo $password1; ?>">
-                    <svg class="password-toggle-icon" onclick="togglePasswordVisibility()" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
-                        <path d="M12 19.7c-2.9 0-5.7-1.1-7.9-3.3a11 11 0 0 1 0-16.4C6.3 1.4 9.1.3 12 .3s5.7 1.1 7.9 3.3A11 11 0 0 1 12 19.7zM12 2.1C9.1 3.6 4.9 7.7 2 12c2.9 4.3 7.1 8.4 10 9.9 2.9-1.6 7.1-5.7 10-9.9-2.9-4.3-7.1-8.4-10-9.9zm0 15.8c-3.4-2.6-7-6.4-9.4-10C4 5.7 7.6 3.9 12 3.9s8 1.8 9.4 4c-2.4 3.6-6 7.4-9.4 10z" />
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="password-toggle-icon bi bi-eye-slash" onclick="togglePasswordVisibility()" viewBox="0 0 16 16">
+                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
                     </svg>
                 </div>
                 <?php if (isset($errors['password'])) echo '<p class="text-danger">' . $errors['password'] . '</p>'; ?>
@@ -116,18 +117,18 @@ if (isset($_POST['submit'])) {
             const toggleIcon = document.querySelector(".password-toggle-icon");
 
             if (passwordInput.type === "password") {
-                passwordInput.type = "text";
-                toggleIcon.innerHTML = `<path d="M12 19.7c-2.9 0-5.7-1.1-7.9-3.3a11 11 0 0 1 0-16.4C6.3 1.4 9.1.3 12 .3s5.7 1.1 7.9 3.3A11 11 0 0 1 12 19.7zM12 2.1C9.1 3.6 4.9 7.7 2 12c2.9 4.3 7.1 8.4 10 9.9 2.9-1.6 7.1-5.7 10-9.9-2.9-4.3-7.1-8.4-10-9.9zm0 15.8c-3.4-2.6-7-6.4-9.4-10C4 5.7 7.6 3.9 12 3.9s8 1.8 9.4 4c-2.4 3.6-6 7.4-9.4 10z" />`;
+                passwordInput.type = 'text';
+                toggleIcon.classList.remove('bi-eye');
+                toggleIcon.classList.add('bi-eye-slash');
             } else {
-                passwordInput.type = "password";
-                toggleIcon.innerHTML = `<path d="M12 7.3a4.7 4.7 0 1 0 0 9.4 4.7 4.7 0 0 0 0-9.4zm0 7.1a2.4 2.4 0 1 1 0-4.8 2.4 2.4 0 0 1 0 4.8zM12 3.5a9.4 9.4 0 1 0 0 18.8 9.4 9.4 0 0 0 0-18.8zm0 16.3A6.9 6.9 0 1 1 12 3.9a6.9 6.9 0 0 1 0 13.9z" />`;
+                passwordInput.type = 'password';
+                toggleIcon.classList.remove('bi-eye-slash');
+                toggleIcon.classList.add('bi-eye');
             }
         }
     </script>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="node_modules\bootstrap\dist\js\bootstrap.bundle.js"></script>
 </body>
 
 </html>
